85f16c2c6b131de2e3351375aafc6f79b29072d3
huyle <lehuuduchuy@gmail.com>
2024-10-29 21:54:21 +0700

    Thêm sản phẩm vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,19 @@
-console.log("ok");
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, 2000);
+}
+
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadPreviewInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadPreviewInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
---
+++ routes/client/cart.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const controller = require("../../controllers/client/cart.controller")
+const router = express.Router();
+
+router.post("/add/:productId", controller.addPost);
+
+module.exports = router;
